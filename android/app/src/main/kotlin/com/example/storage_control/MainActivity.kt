c0df618cb202d9b08b02e78f3d3c293b05cbc67b
Pedro H. Alarcon <pedrohenriquealarcon@gmail.com>
2023-08-07 23:02:43 -0300

    Criado estrutura inicial de pastas - 2023.8.7.2

--- android/app/src/main/kotlin/com/example/storage_control/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.storage_control
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ lib/classes/cores.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Cores {
+  static const Color vermelho = Color(0xFFE53935);
+  static const Color vermelhoEscuro = Color(0xFFC62828);
+  static const Color vermelhoClaro = Color(0xFFFFEBEE);
+  static const Color branco = Color(0xFFFFFFFF);
+  static const Color preto = Color(0xFF212121);
+}
