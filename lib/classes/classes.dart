6b4e8dd433ee759a24342ed66382105246432000
Pedro H. Alarcon <pedrohenriquealarcon@gmail.com>
2023-08-08 23:09:02 -0300

    Adicionado barra lateral de navegação - 2023.8.8.1

---
+++ lib/classes/classes.dart
@@ -0,0 +1,1 @@
+export "Cores.dart";
--- lib/classes/cores.dart
+++ lib/classes/cores.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 class Cores {
   static const Color vermelho = Color(0xFFE53935);
-  static const Color vermelhoEscuro = Color(0xFFC62828);
+  static const Color vermelhoEscuro = Color.fromARGB(255, 153, 23, 14);
   static const Color vermelhoClaro = Color(0xFFFFEBEE);
   static const Color branco = Color(0xFFFFFFFF);
   static const Color preto = Color(0xFF212121);
+  static const Color transparente = Color(0x00000000);
 }
