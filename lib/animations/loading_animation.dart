45fbc8807ac903c6a28a4a5606a755b9a0ce5300
Pedro H. Alarcon <pedrohenriquealarcon@gmail.com>
2023-09-01 21:52:58 -0300

    Iniciando criação de animaão personalizada - 2023.9.1.1

--- lib/animations/loading_animation.dart
+++ lib/animations/loading_animation.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LoadingAnimation extends StatefulWidget {
@@ -7,9 +8,69 @@ class LoadingAnimation extends StatefulWidget {
   State<LoadingAnimation> createState() => _LoadingAnimationState();
 }
 
-class _LoadingAnimationState extends State<LoadingAnimation> {
+class _LoadingAnimationState extends State<LoadingAnimation>
+    with TickerProviderStateMixin {
+  late AnimationController _animationController;
+  late IconData icon;
+  int tempo = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(startAnimation(), size: 40),
+        const SizedBox(height: 10),
+        const Text('Carregando...'),
+      ],
+    );
+  }
+
+  IconData startAnimation() {
+    tempo <= 500
+        ? setState(() {
+            icon = CupertinoIcons.hourglass_bottomhalf_fill;
+          })
+        : tempo > 500 && tempo <= 1000
+            ? setState(() {
+                icon = CupertinoIcons.hourglass;
+              })
+            : tempo > 1000 && tempo <= 1500
+                ? setState(() {
+                    icon = CupertinoIcons.hourglass_tophalf_fill;
+                  })
+                : setState(() => tempo = 0);
+    //   AnimatedBuilder(
+    //     animation: _animationController,
+    //     builder: (context, child) {
+    //       return Transform.rotate(
+    //         angle: _animationController.value * 6.3,
+    //         child: child,
+    //       );
+    //     },
+    //     child: Icon(icon, size: 40),
+    //   );
+    //   _animationController.forward();
+    return icon;
+  }
+
+  void correrTempo() {
+    Future.delayed(const Duration(milliseconds: 1), () {
+      if (tempo <= 2000) {
+        setState(() {
+          tempo += 1;
+        });
+      } else {
+        setState(() {
+          tempo = 0;
+        });
+      }
+    });
   }
 }
--- lib/pages/listar_quartos_page.dart
+++ lib/pages/listar_quartos_page.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/src/widgets/basic.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:storage_control/animations/loading_animation.dart';
 
 class ListarQuartosPage extends StatefulWidget {
   const ListarQuartosPage({super.key});
@@ -11,6 +13,6 @@ class ListarQuartosPage extends StatefulWidget {
 class _ListarQuartosPageState extends State<ListarQuartosPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Center(child: LoadingAnimation());
   }
 }
