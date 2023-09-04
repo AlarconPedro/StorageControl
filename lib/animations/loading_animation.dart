48782ab542988d9379c55dc168915648028fe0a9
Pedro H. Alarcon <pedrohenriquealarcon@gmail.com>
2023-09-04 19:01:59 -0300

    Adicionado nova funcionalidade na animação do icone - 2023.9.4.1

--- lib/animations/loading_animation.dart
+++ lib/animations/loading_animation.dart
@@ -11,13 +11,14 @@ class LoadingAnimation extends StatefulWidget {
 class _LoadingAnimationState extends State<LoadingAnimation>
     with TickerProviderStateMixin {
   late AnimationController _animationController;
-  late IconData icon;
+  IconData icon = CupertinoIcons.hourglass_bottomhalf_fill;
   int tempo = 0;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    animateIcon();
   }
 
   @override
@@ -25,51 +26,35 @@ class _LoadingAnimationState extends State<LoadingAnimation>
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Icon(startAnimation(), size: 40),
+        Icon(icon, size: 40),
         const SizedBox(height: 10),
         const Text('Carregando...'),
       ],
     );
   }
 
-  IconData startAnimation() {
-    tempo <= 500
-        ? setState(() {
-            icon = CupertinoIcons.hourglass_bottomhalf_fill;
-          })
-        : tempo > 500 && tempo <= 1000
-            ? setState(() {
-                icon = CupertinoIcons.hourglass;
-              })
-            : tempo > 1000 && tempo <= 1500
-                ? setState(() {
-                    icon = CupertinoIcons.hourglass_tophalf_fill;
-                  })
-                : setState(() => tempo = 0);
-    //   AnimatedBuilder(
-    //     animation: _animationController,
-    //     builder: (context, child) {
-    //       return Transform.rotate(
-    //         angle: _animationController.value * 6.3,
-    //         child: child,
-    //       );
-    //     },
-    //     child: Icon(icon, size: 40),
-    //   );
-    //   _animationController.forward();
-    return icon;
-  }
-
-  void correrTempo() {
-    Future.delayed(const Duration(milliseconds: 1), () {
-      if (tempo <= 2000) {
+  animateIcon() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+    _animationController.forward();
+    _animationController.addListener(() {
+      if (_animationController.isCompleted) {
         setState(() {
-          tempo += 1;
+          tempo++;
+          if (tempo % 2 == 0) {
+            icon = CupertinoIcons.hourglass_tophalf_fill;
+          } else {
+            icon = CupertinoIcons.hourglass;
+          }
+          if (tempo == 3) {
+            setState(() {
+              tempo = 0;
+            });
+          }
         });
-      } else {
-        setState(() {
-          tempo = 0;
-        });
+        // _animationController.reset();
       }
     });
   }
